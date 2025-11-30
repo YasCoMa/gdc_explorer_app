d38f8693c24568dc8238528d803d773475246038
yasmmin <ycfrenchgirl2@gmail.com>
2025-11-30 14:26:42 +0100

    update

--- draft.js
+++ draft.js
@@ -22,51 +22,59 @@ dat.hits.forEach( el => {
 	let uuid = el.file_id;
 	let size = el.file_size;
 	let platform = el.platform;
-	let sample_id = el.cases[0].submiter_id;
-	let oaux = { 'uuid': uuid, 'submitter_id': sample_id, "size": size, "platform": platform };
-	let sobj = { "sample_id": sample_id };
 
-	let tissue = el.samples[0].tissue_type;
-	let stage = el.samples[0].tumor_descriptor;
-	y[uuid] = { "tissue": tissue, "stage": stage };
+	try{
+		let sample_id = el.cases[0].submitter_id;
+		let oaux = { 'uuid': uuid, 'submitter_id': sample_id, "size": size, "platform": platform };
+		let sobj = { "sample_id": sample_id };
 
-	let races = Object.keys( filesdf['race'] );
-	let dr = e.cases[0].demographic.race;
-	sobj["race"] = dr;
-	if( ! races.includes(dr) ){
-		filesdf['race'][dr] = [];
-	}
-	filesdf['race'][dr].push( oaux );
+		let tissue = el.cases[0].samples[0].tissue_type;
+		let stage = el.cases[0].samples[0].tumor_descriptor;
+		y[uuid] = { "tissue": tissue, "stage": stage };
 
-	let genders = Object.keys( filesdf['gender'] );
-	dr = e.cases[0].demographic.gender;
-	sobj["gender"] = dr;
-	if( ! genders.includes(dr) ){
-		filesdf['gender'][dr] = [];
-	}
-	filesdf['gender'][dr].push( oaux );
-
-	let ethncities = Object.keys( filesdf['ethnicity'] );
-	dr = e.cases[0].demographic.ethnicity;
-	sobj["ethnicity"] = dr;
-	if( ! ethncities.includes(dr) ){
-		filesdf['ethnicity'][dr] = [];
-	}
-	filesdf['ethnicity'][dr].push( oaux );
-
-	samples['uuid'] = sobj;
-
-	let diags = el.cases[0].diagnosis;
-	for( let dg of diags ){
-		if( dg.days_to_recurrence ){
-			dg.age_at_diagnosis = parseInt( dg.age_at_diagnosis / 365 );
-			dg.days_to_recurrence = parseInt( dg.days_to_recurrence / 365 );
-			dg.days_to_last_follow_up = parseInt( dg.days_to_last_follow_up / 365 );
-			let obj = dg;
-			obj['uuid'] = uuid;
-			obj['submitter_id'] = sample_id;
-			recurrents.push( obj );
+		let races = Object.keys( filesdf['race'] );
+		let dr = el.cases[0].demographic.race;
+		sobj["race"] = dr;
+		if( ! races.includes(dr) ){
+			filesdf['race'][dr] = [];
 		}
+		filesdf['race'][dr].push( oaux );
+
+		let genders = Object.keys( filesdf['gender'] );
+		dr = el.cases[0].demographic.gender;
+		sobj["gender"] = dr;
+		if( ! genders.includes(dr) ){
+			filesdf['gender'][dr] = [];
+		}
+		filesdf['gender'][dr].push( oaux );
+
+		let ethncities = Object.keys( filesdf['ethnicity'] );
+		dr = el.cases[0].demographic.ethnicity;
+		sobj["ethnicity"] = dr;
+		if( ! ethncities.includes(dr) ){
+			filesdf['ethnicity'][dr] = [];
+		}
+		filesdf['ethnicity'][dr].push( oaux );
+
+		samples[uuid] = sobj;
+
+		let diags = el.cases[0].diagnoses;
+		for( let dg of diags ){
+			console.log(dg)
+			if( dg.days_to_recurrence ){
+				dg.age_at_diagnosis = parseInt( dg.age_at_diagnosis / 365 );
+				dg.days_to_recurrence = parseInt( dg.days_to_recurrence / 365 );
+				dg.days_to_last_follow_up = parseInt( dg.days_to_last_follow_up / 365 );
+				let obj = dg;
+				obj['uuid'] = uuid;
+				obj['submitter_id'] = sample_id;
+				recurrents.push( obj );
+			}
+		}
+	}
+	catch(e){
+		console.log(el)
+		console.log('error', e)
 	}
 
 } )
