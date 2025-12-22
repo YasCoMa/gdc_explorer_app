8726c6218a9ad1f82bc109d9746801a4d0be10b0
yasmmin <ycfrenchgirl2@gmail.com>
2025-12-22 23:42:11 +0100

    snv

--- gesel.js-master/tests/fetchSetsForAllGenes.test.js
+++
@@ -1,7 +0,0 @@
-import * as utils from "./utils.js";
-import * as gesel from "../src/index.js"
-
-test("fetching sets for all genes works correctly", async () => {
-    let full = await gesel.fetchSetsForAllGenes("10090");
-    expect(full.length).toEqual((await gesel.fetchAllGenes("10090")).get("ensembl").length); 
-})
