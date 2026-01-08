c2ae133a562c48934df939849e569f11a9c96b24
yasmmin <ycfrenchgirl2@gmail.com>
2026-01-08 23:13:38 +0100

    expai2

--- processing_scripts/handle_geneset_enrichment.py
+++ processing_scripts/handle_geneset_enrichment.py
@@ -637,15 +637,15 @@ class HandleEnrichment:
         #self.enrich_exclusive_mutated_genes(p)
         
         p = 'TCGA-READ'
-        self.perform_degs_localEnrichment_simulation(p)
+        #self.perform_degs_localEnrichment_simulation(p)
 
         #d = self._get_info_clinvar('rs334')
         #print(d)
 
         projects = [ "TCGA-ACC",  "TCGA-BLCA",  "TCGA-BRCA",  "TCGA-CESC",  "TCGA-CHOL",  "TCGA-COAD",  "TCGA-DLBC",  "TCGA-ESCA",  "TCGA-GBM",  "TCGA-HNSC",  "TCGA-KICH",  "TCGA-KIRC",  "TCGA-KIRP",  "TCGA-LAML",  "TCGA-LGG",  "TCGA-LIHC",  "TCGA-LUAD",  "TCGA-LUSC",  "TCGA-MESO",  "TCGA-OV",  "TCGA-PAAD",  "TCGA-PCPG",  "TCGA-PRAD",  "TCGA-READ",  "TCGA-SARC",  "TCGA-SKCM",  "TCGA-STAD",  "TCGA-TGCT",  "TCGA-THCA",  "TCGA-THYM",  "TCGA-UCEC",  "TCGA-UCS",  "TCGA-UVM" ]
-        #for p in tqdm(projects):
-            #self.perform_degs_analysis_simulation(p)
-            #self.perform_degs_localEnrichment_simulation(p)
+        for p in tqdm(projects):
+            self.perform_degs_analysis_simulation(p)
+            self.perform_degs_localEnrichment_simulation(p)
 
 if( __name__ == "__main__" ):
     o = HandleEnrichment()
